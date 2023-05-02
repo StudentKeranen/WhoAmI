b834e8976f9ccaf9770d9e8fae46abc41e9e2d27
StudentKeranen <s2108237@student.hb.se>
2023-05-02 14:29:40 +0200

    Lagt till en constructor och en Save metod i UnitOfWork

--- Datalayer/UnitOfWork.cs
+++ Datalayer/UnitOfWork.cs
@@ -9,7 +9,20 @@ namespace Datalayer
 {
     public class UnitOfWork
     {
+        protected AppDbContext appDbContext { get; }
         public Repository<User> UserRepository { get; private set; } = null!;
         public Repository<Company> CompanyRepository { get; private set; } = null!;
+        public UnitOfWork()
+        {
+            appDbContext = new AppDbContext();
+
+            UserRepository = new Repository<User>(appDbContext);
+            CompanyRepository = new Repository<Company>(appDbContext);
+        }
+
+        public void Save()
+        {
+            appDbContext.SaveChanges();
+        }
     }
 }

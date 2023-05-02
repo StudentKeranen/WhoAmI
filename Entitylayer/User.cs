3014645acc0c2d24931c53952f6f9b65e2bd5643
Jonathan Öberg <s2105325@student.hb.se>
2023-05-02 11:14:00 +0200

    updaterat synligheten för User

--- Entitylayer/User.cs
+++ Entitylayer/User.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Entitylayer
 {
-    internal class User
+    /// <summary>
+    /// Following usertypes is avaliable 1=Admin 2=User
+    /// </summary>
+    public class User
     {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        private int _userTypeValue;
+        public int Usertype 
+        {
+            get{return _userTypeValue;}
+            
+            set
+            {
+                if (value < 1 || value > 2)
+                {
+                    throw new ArgumentOutOfRangeException((nameof(Usertype)));
+                }
+
+                _userTypeValue = value;
+            }
+        }
+
+        public User(string firstname, string lastName, string password, int usertype)
+        {
+            this.Firstname =firstname;
+            this.Surname = lastName;
+            this.Password = password;
+            this.Usertype = usertype;
+        }
     }
 }

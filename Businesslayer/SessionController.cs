a4fbae5f72bdac5b400dd7d6de69a867b7ea9289
Jonathan Öberg <s2105325@student.hb.se>
2023-05-02 11:12:51 +0200

    Skapat upp User-klassen i entitetslagret och skapat en SessionController. Även lagt till referencer mellan Businesslayer och Entitylayer.

--- Businesslayer/SessionController.cs
+++ Businesslayer/SessionController.cs
@@ -1,9 +1,25 @@
 using System;
+using Entitylayer;
 
 namespace Businesslayer
 {
     public class SessionController
     {
-        Inloggad() //Ska byggas klart
+        public UnitOfWork unitOfWork = new UnitOfWork(); //Logiken/kontext lägger vi i UoW-klassen
+        public User loggedIn { get; set; }
+
+        public User LogIn(string firstname, string password)
+        {
+            User credentials = unitOfWork.UserRepository.FirstOrDefault(u => u.UserId == Firstname);
+            if (credentials != null && credentials.Password.Equals(password)) // Kollar att expedit e hittades och att lösenord stämmer överens
+            {
+                return credentials;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }

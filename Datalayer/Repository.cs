e1928c52155ddb6594a3eada3a6c338693808402
Jonathan Öberg <s2105325@student.hb.se>
2023-05-02 12:41:29 +0200

    update

--- Datalayer/Repository.cs
+++ Datalayer/Repository.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
-namespace Datalayer
+namespace DataLayer
 {
-    internal class Repository
+    public class Repository<T>
+        where T : class
     {
+        internal SessionContext context;
+        internal DbSet<T> dbSet;
+
+        public Repository(SessionContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+
+        /// <summary>
+        ///  Add a new entity to the Table.
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+
+
+        }
+        /// <summary>
+        ///  Find a set of entities that match a predicate.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return dbSet.Where(predicate);
+        }
+        /// <summary>
+        ///  Find the first entity that match a predicate.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public T FirstOrDefault(Func<T, bool> predicate)
+        {
+            return dbSet.FirstOrDefault(predicate);
+        }
+        public IEnumerable<T> Find(Func<T, bool> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            var query = dbSet.AsQueryable();
+            foreach (var include in includes)
+                query = query.Include(include);
+            return query.Where(predicate);
+        }
+        public T FirstOrDefault(Func<T, bool> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            var query = dbSet.AsQueryable();
+            foreach (var include in includes)
+                query = query.Include(include);
+            return query.FirstOrDefault(predicate);
+        }
     }
 }

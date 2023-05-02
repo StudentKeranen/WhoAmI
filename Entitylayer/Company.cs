d902377d56358a90d989e0ae1e18e128e9a961c6
StudentKeranen <s2108237@student.hb.se>
2023-05-02 09:30:03 +0200

    Lagt till CompanyID och Description

--- Entitylayer/Company.cs
+++ Entitylayer/Company.cs
@@ -4,9 +4,10 @@ namespace Entitylayer
 {
     public class Company
     {
+        public int CompanyId { get; set; }
         public string CompanyName { get; set; }
-
         public int CompanyNumber { get; set; }
-
+        public string CompanyDescription { get; set;}
+        
     }
 }

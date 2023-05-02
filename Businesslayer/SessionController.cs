59f0ce1b38fd18159424dc05f22aa1976a929fc1
Jonathan Öberg <s2105325@student.hb.se>
2023-05-02 11:52:37 +0200

    Adderat UnitOfWork-klassen i datalagret samt byggt en singelton i affärslagret för inloggning.

--- Businesslayer/SessionController.cs
+++ Businesslayer/SessionController.cs
@@ -4,21 +4,44 @@ using Entitylayer;
 
 namespace Businesslayer
 {
-    public class SessionController
+    public sealed class SessionController
     {
+        private static SessionController? _instance;
+
+        private SessionController(User user) 
+        {
+            LoggedIn = user;
+        }
+
+        public static SessionController Instance(User user)
+        {
+            if (_instance == null)
+            {
+                _instance = new SessionController(user);
+            }
+            return _instance;
+        }
+        public static SessionController Instance()
+        {
+            if (null == _instance)
+            {
+                throw new ApplicationException("No user found!");
+            }
+            return _instance;
+        }
         public UnitOfWork unitOfWork = new UnitOfWork(); //Logiken/kontext lägger vi i UoW-klassen
-        private User? loggedIn { get; set; }
+        private User? LoggedIn { get; set; }
 
         public void LogIn(string firstname, string password)
         {
             User credentials = unitOfWork.UserRepository.FirstOrDefault(u => u.UserId == Firstname);
             if (credentials != null && credentials.Password.Equals(password)) // Kollar att expedit e hittades och att lösenord stämmer överens
             {
-                loggedIn = credentials;
+                LoggedIn = credentials;
             }
             else
             {
-                loggedIn = null;
+                LoggedIn = null;
             }
         }
 

8cca88daf1f2f4b299312151ccc269063847b99e
Jonathan Öberg <s2105325@student.hb.se>
2023-05-02 12:41:08 +0200

    UoW och repository

--- Businesslayer/SessionController.cs
+++ Businesslayer/SessionController.cs
@@ -36,7 +36,7 @@ namespace Businesslayer
         public void LogIn(string firstname, string password)
         {
             User credentials = unitOfWork.UserRepository.FirstOrDefault(u => u.UserId == Firstname);
-            if (credentials != null && credentials.Password.Equals(password)) // Kollar att expedit e hittades och att lösenord stämmer överens
+            if (credentials != null && credentials.Password.Equals(password))
             {
                 LoggedIn = credentials;
             }

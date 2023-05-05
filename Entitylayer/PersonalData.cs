d1cdd87038f37b8373dc331b3987fcf8468b5cb2
StudentKeranen <s2108237@student.hb.se>
2023-05-05 09:54:53 +0200

    Lagt till Entiteterna DataCategory och PersonalData

---
+++ Entitylayer/PersonalData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitylayer
+{
+    public class PersonalData
+    {
+        public int PersonalDataId { get; set; }
+        public bool FoundData { get; set; }
+        public string DataName { get; set; }
+        public DataCategory DataCategories { get; set; }
+    }
+}

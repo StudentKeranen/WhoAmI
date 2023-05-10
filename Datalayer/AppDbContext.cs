d96c98df88e8c713893bc9394f79fd891badb61c
StudentKeranen <s2108237@student.hb.se>
2023-05-10 09:21:28 +0200

    Flyttat bilderna i försöka att få dem att rendera. Funkar dock inte!

--- Datalayer/AppDbContext.cs
+++ Datalayer/AppDbContext.cs
@@ -30,27 +30,28 @@ namespace Datalayer
         public void seed()
         {
             Database.EnsureDeleted();
-            Database.EnsureCreated();
+            if (Database.EnsureCreated())
+            {
+                DataCategory category = new DataCategory() { CategoryName = "Personal Information", CategoryDescription = "Data that are closely connected to the individual person" };
+                DataCategories.Add(category);
 
-            DataCategory category = new DataCategory() { CategoryName = "Personal Information", CategoryDescription = "Data that are closely connected to the individual person"};
-            DataCategories.Add(category);
-            
-            User user = new User("sOlstrale", 2);
-            Users.Add(user);
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Jonathan", User = user });
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Öberg", User = user }); 
-            
-            user = new User("SaSforLajf", 1);
-            Users.Add(user);
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Robert", User = user });
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Keränen", User = user });
-            
-            user = new User("KingsbackHood", 1);
-            Users.Add(user);
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Danny", User = user });
-            PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Mansour", User = user });
+                User user = new User("sOlstrale", 2);
+                Users.Add(user);
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Jonathan", User = user });
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Öberg", User = user });
 
-            SaveChanges();
+                user = new User("SaSforLajf", 1);
+                Users.Add(user);
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Robert", User = user });
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Keränen", User = user });
+
+                user = new User("KingsbackHood", 1);
+                Users.Add(user);
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "First name", DataValue = "Danny", User = user });
+                PersonalDataSet.Add(new PersonalData() { DataCategories = category, DataName = "Last name", DataValue = "Mansour", User = user });
+
+                SaveChanges();
+            }
         }
     }
 }
